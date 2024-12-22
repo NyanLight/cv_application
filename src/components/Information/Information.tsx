7f571504741f13262b8bd5e9d24d758f927b7073
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-12-22 23:18:16 +0400

    create information component

---
+++ src/components/Information/Information.tsx
@@ -0,0 +1,29 @@
+import { cvProps } from "../../types";
+
+export function information (props: cvProps) {
+    return (
+        <div className="information">
+            <section>
+                <h3>Contact information</h3>
+                Name: {props.name}
+                Email: {props.email}
+                Phone: {props.phone}
+            </section>
+            <section>
+                <h3>Education information</h3>
+                School name: {props.schoolName}
+                Title of study: {props.studyTitle}
+                Date of graduation: {props.studyEndDate.toString()}
+            </section>
+            <section>
+                <h3>Practical experience</h3>
+                Company name: {props.companyName}
+                Position title: {props.positionTitle}
+                Main responsibilities: {props.responsibilities}
+                Job start date: {props.jobStartDate.toString()}
+                Job end date: {props.jobEndDate.toString()}
+            </section>
+
+        </div>
+    )
+}
