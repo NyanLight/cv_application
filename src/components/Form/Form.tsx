d188c438c756e1a78b97e15720cce6a01d5ca4c8
NyanLight <128150876+NyanLight@users.noreply.github.com>
2024-12-23 03:04:24 +0400

    minor style changes

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,24 +1,25 @@
 import { Input } from "../../ui/Input/Input";
 import { Textarea } from "../../ui/Textarea/Textarea";
 import { FormProps } from "./Form.types";
+import styles from "./Form.module.css"
 
 export function Form (props: FormProps) {
     return (
-        <form action="">
-            <section>
-                <h3>Contact information</h3>
+        <form action="" className={styles.form}>
+            <section className={styles.section}>
+                <h3 className={styles.sectionTitle}>Contact information</h3>
                 <Input name="name" value={props.name} label="Name" inputType="text" handler={props.handler}></Input>
                 <Input name="email" value={props.email} label="Email" inputType="email" handler={props.handler}></Input>
                 <Input name='phone'  value={props.phone} label="Phone" inputType="tel" handler={props.handler}></Input>
             </section>
-            <section>
-                <h3>Education information</h3>
+            <section className={styles.section}>
+                <h3 className={styles.sectionTitle}>Education information</h3>
                 <Input name="schoolName" value={props.schoolName} label="School name" inputType="text" handler={props.handler}></Input>
                 <Input name="studyTitle" value={props.studyTitle} label="Title of study" inputType="text" handler={props.handler}></Input>
                 <Input name='studyEndDate'  value={props.studyEndDate} label="Graduation date" inputType="date" handler={props.handler}></Input>
             </section>
-            <section>
-                <h3>Practical experience</h3>
+            <section className={styles.section}>
+                <h3 className={styles.sectionTitle}>Practical experience</h3>
                 <Input name="companyName" value={props.companyName} label="Company name" inputType="text"  handler={props.handler}></Input>
                 <Input name="positionTitle" value={props.positionTitle} label="Position title" inputType="text" handler={props.handler}></Input>
                 <Textarea name="responsibilities" value={props.responsibilities} label="Main responsibilities" handler={props.handler}></Textarea>
--- src/ui/Textarea/Textarea.tsx
+++ src/ui/Textarea/Textarea.tsx
@@ -1,9 +1,10 @@
-import { TextareaProps } from "./Textarea.types"
+import { TextareaProps } from "./Textarea.types";
+import styles from "./Textarea.module.css";
 
-export function Textarea ({label, handler, name}: TextareaProps) {
+export function Textarea ({label, handler, name, value}: TextareaProps) {
     return (
-        <label >
-            {label}: <textarea name={name} onChange={handler} placeholder="You should write something"></textarea>
+        <label className={styles.label} >
+            {label}: <textarea value={value} className={styles.textarea} rows={5} name={name} onChange={handler} placeholder="You should write something"></textarea>
         </label>
     )
 }
