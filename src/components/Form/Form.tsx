2e891ab691b3430a59bee404907f7f5bfa5e334b
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-09 22:31:02 +0400

    new styling for form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -7,19 +7,28 @@ export function Form (props: FormProps) {
     return (
         <form action="" className={styles.form}>
             <section className={styles.section}>
-                <h3 className={styles.sectionTitle}>Contact information</h3>
+                <div className={styles.titleWrapper}>
+                    <img className={styles.icon} src="src/assets/contact.svg" alt="" />
+                    <h3 className={styles.sectionTitle}>Contact information</h3>
+                </div>
                 <Input name="name" value={props.name} label="Name" inputType="text" handler={props.handler} />
                 <Input name="email" value={props.email} label="Email" inputType="email" handler={props.handler} />
                 <Input name='phone'  value={props.phone} label="Phone" inputType="tel" handler={props.handler} />
             </section>
             <section className={styles.section}>
-                <h3 className={styles.sectionTitle}>Education information</h3>
+                <div className={styles.titleWrapper}>
+                    <img   className={styles.icon} src="src/assets/education.svg" alt="" />
+                    <h3 className={styles.sectionTitle}>Education information</h3>
+                </div>
                 <Input name="schoolName" value={props.schoolName} label="School name" inputType="text" handler={props.handler} />
                 <Input name="studyTitle" value={props.studyTitle} label="Title of study" inputType="text" handler={props.handler} />
                 <Input name='studyEndDate'  value={props.studyEndDate} label="Graduation date" inputType="date" handler={props.handler} />
             </section>
             <section className={styles.section}>
-                <h3 className={styles.sectionTitle}>Practical experience</h3>
+                <div className={styles.titleWrapper}>
+                    <img  className={styles.icon} src="src/assets/work.svg" alt="" />
+                    <h3 className={styles.sectionTitle}>Practical experience</h3>
+                </div>
                 <Input name="companyName" value={props.companyName} label="Company name" inputType="text"  handler={props.handler} />
                 <Input name="positionTitle" value={props.positionTitle} label="Position title" inputType="text" handler={props.handler} />
                 <Textarea name="responsibilities" value={props.responsibilities} label="Main responsibilities" handler={props.handler} />
