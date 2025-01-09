c7db53e7158a5e6ad672a5399daa155111d9dcba
NyanLight <128150876+NyanLight@users.noreply.github.com>
2025-01-09 21:57:07 +0400

    add new styling to information component

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -4,26 +4,43 @@ import styles from "./Information.module.css"
 export function Information (props: CV) {
     return (
         <div className={styles.information}>
-            <section className={styles.section}>
-                <h3 className={styles.title} >Contact information</h3>
-                <div className={styles.field}><span className={styles.fieldName}>Name:</span> {props.name}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Email:</span> {props.email}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Phone:</span> {props.phone}</div>
+            <section className={styles.header}>
+                <h1>{props.name}</h1>
             </section>
-            <section className={styles.section}>
-                <h3 className={styles.title}>Education information</h3>
-                <div className={styles.field}><span className={styles.fieldName}>School name:</span> {props.schoolName}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Title of study:</span> {props.studyTitle}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Date of graduation:</span> {props.studyEndDate.toString()}</div>
-            </section>
-            <section className={styles.section}>
-                <h3 className={styles.title}>Practical experience</h3>
-                <div className={styles.field}><span className={styles.fieldName}>Company name:</span> {props.companyName}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Position title:</span> {props.positionTitle}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Main responsibilities:</span> {props.responsibilities}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Job start date:</span> {props.jobStartDate.toString()}</div>
-                <div className={styles.field}><span className={styles.fieldName}>Job end date:</span> {props.jobEndDate.toString()}</div>
-            </section>
+            <div className={styles.wrapper}>
+                <section className={styles.contacts}>
+                    <h2 className={styles.information__title}>Contacts</h2>
+                    <div className={styles.contact}>
+                        <img className={styles.contact__icon} src="src/assets/email.svg" alt="" />
+                        <div>{props.email}</div>
+                    </div>
+                    <div className={styles.contact}>
+                        <img className={styles.contact__icon} src='src/assets/phone.svg' alt="" />
+                        <div>{props.phone}</div>
+                    </div>
+                </section>
+                <section className={styles.main}>
+                    <div className={styles.main__education}>
+                        <h2 className={styles.information__title}>Education</h2>
+                        <div>
+                            <div className={styles.education__studyTitle}>{props.studyTitle.toUpperCase()}</div>
+                            <div className={styles.education__institution}>
+                                <span className={styles.education__schoolName}>{props.schoolName}</span> | <span className={styles.education__endYear}>Graduated in {props.studyEndDate}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.main__experience}>
+                        <h2 className={styles.information__title}>Experience</h2>
+                        <div>
+                            <div className={styles.experience__positionTitle}>{props.positionTitle.toUpperCase()}</div>
+                            <div>
+                                <span className={styles.experience__companyName}>{props.companyName}</span> | <span className={styles.experience__years}>{props.jobStartDate} - {props.jobEndDate}</span>
+                            </div>
+                            <div className={styles.experience__responsibilities}>{props.responsibilities}</div>
+                        </div>
+                    </div>
+                </section>
+            </div>
 
         </div>
     )
